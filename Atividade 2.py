0d05ee86c038140df9c8289b85366eba94cbf74c
andrerosa01 <129331575+andrerosa01@users.noreply.github.com>
2023-05-31 17:40:35 -0300

    Add files via upload
    Professor tive um pouco de dificuldade, mas acho que atendeu ao que pediu...

---
+++ Atividade 2.py
@@ -0,0 +1,10 @@
+lista = [2, 3, 7, 12, 2]
+
+print("Números da lista:")
+for num in lista:
+    print(num)
+
+# Imprimir os números multiplicados por 2
+print("\nNúmeros multiplicados por 2:")
+for num in lista:
+    print(num * 2)
